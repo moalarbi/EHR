11afb8f02747fcebae8146f88f910f6be5d07c27
moalarbi <moalarbi@example.com>
2026-04-14 09:58:24 -0400

    Update API_URL to the latest staging backend

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbw8Jf-V2otLwiSG9if0BafIP_Qbzje6VgrMD4YuypkILLBnR8gjyqu_dqzkiLNjyXFi/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzfXhB0Y5EcVkbvi4UuPSRIx3468WNqjW0eEgVPqP71qv1b6KDzmsPBk4NtV2s3gCLD/exec',
   APP_NAME_AR: 'نظام تسجيل الإيرادات اليومية',
   APP_NAME_EN: 'Daily Revenue Recording System'
 };
